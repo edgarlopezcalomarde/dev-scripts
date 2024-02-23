231d735bb3bf00fb15a472482687d069eca0bb84
Edgar Lopez Calomarde <edgarlopezcalomarde@gmail.com>
2024-02-23 08:59:14 +0100

    rehype codeblock added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,35 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const prettyCodeOptions = {
+  theme: "dark-plus",
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word"];
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
   base: "/dev-scripts",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown:{
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  }
 });
